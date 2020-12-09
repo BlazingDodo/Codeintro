23adced9ae485b22b6b7245a1f3cc1ebbec9beec
“BlazingDodo git config --global user.email aryanmodi.1999@gmail.com git config --global user.name “BlazingDodo <aryanmodi.1999@gmail.com>
2020-12-10 02:45:32 +0530

    Commit to Github

--- Codeintro/ViewController.swift
+++ Codeintro/ViewController.swift
@@ -9,12 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
+   
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
 
+    var backgroundColor: UIColor!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+   
+        backgroundColor = view.backgroundColor
+    
     }
 
+    @IBAction func Tapbutton(_ sender: Any) {
+    print("Hello")
+        textLabel.textColor = UIColor.white
+  
+    }
+    
+    @IBAction func Tapviewbutton(_ sender: Any) {
+        view.backgroundColor = UIColor.systemPink
+    
+    }
 
+    @IBAction func Taptextbutton(_ sender: Any) {
+        
+       // textLabel.text = "Goodbye!"
+        textLabel.text = textField.text
+        textField.text = ""
+        view.endEditing(true)
+    }
+    
+    @IBAction func OnResetGesture(_ sender: Any) {
+    textLabel.text = "Hello"
+    view.backgroundColor = backgroundColor
+    
+    }
+    
 }
 
